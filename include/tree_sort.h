51454118345713186b33d739d017771df87329e0
chris389838 <chris389838@icloud.com>
2019-09-22 22:52:59 +0300

    Array

---
+++ include/tree_sort.h
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#pragma once
+#include <iostream>
+
+struct Node
+{
+	int field;
+	Node *left;
+	Node *right;
+};
+
+// output of tree nodes(traversal in infix form)
+void print(Node *Tree);
+
+// adding nodes to a tree
+Node * addnode(int data, Node *Tree);
+
+// free tree memory
+void free(Node *Tree);
+
+
+/// to call	-->	put it in	main.cpp
+
+/*Node *root = nullptr;
+	int nod;            // node value
+	for (int i = 0; i < sizeOfArray; i++)
+	{
+		std::cin >> nod;
+		root = addnode(nod, root); // place the entered node on the tree
+	}
+	print(root);
+	free(root);	*/
---
+++ src/tree_sort.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "tree_sort.h"
+
+
+void print(Node *Tree)
+{
+	if (Tree != NULL) {      
+		print(Tree->left);
+		std::cout << Tree->field << " ";
+		print(Tree->right);
+	}
+}
+
+Node * addnode(int data, Node *Tree) {
+	if (Tree == NULL)     // if there is no tree, then form the root
+	{
+		Tree = new Node;	// node memory
+		Tree->field = data;	// data field
+		Tree->left = NULL;
+		Tree->right = NULL; // initialize the branches with a void
+	}
+	else    
+		if (data < Tree->field)   // if the element is less than the root, we go to the left
+			Tree->left = addnode(data, Tree->left); // add an element recursively
+		else  // otherwise go right
+			Tree->right = addnode(data, Tree->right); // add an element recursively
+	return(Tree);
+}
+
+void free(Node *Tree)
+{
+	if (Tree != NULL)    // if the tree is not empty
+	{
+		free(Tree->left);   // recursively delete the left branch
+		free(Tree->right);  // recursively delete the right branch
+		delete Tree;        // remove the root
+	}
+}
