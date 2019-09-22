d90d97bc7889b1a882722125fd9818d3f8881520
chris389838 <chris389838@icloud.com>
2019-09-22 20:38:33 +0300

    ArraySort

---
+++ counting_sort.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include "counting_sort.h"
+
+void countingSort(int sizeOfArray, int *array, int *sortedArray)
+{
+	int pos;
+	for (int i = 0; i < sizeOfArray; i++)
+	{
+		pos = 0;
+		for (int j = 0; j < sizeOfArray; j++)
+		{
+			if (array[i] > array[j])
+				pos++;
+		}
+		sortedArray[pos] = array[i];
+	}
+}
---
+++ include/counting_sort.h
@@ -0,0 +1,7 @@
+#include "pch.h"
+#pragma once
+#include <iostream>
+
+void countingSort(int sizeOfArray, int *array, int *sortedArray);
+
+// to call	-->	countingSort(sizeOfArray, array, sortedArray);
